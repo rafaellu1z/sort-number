74825c2ab14e3938616896515151bf9ff5f452c8
Rafael Luiz <rafael.s.luiz@gmail.com>
2021-06-09 20:58:05 -0300

    Adicionando imagens para o jogo

--- app/src/main/java/com/devventure/sortnumber/MainActivity.kt
+++ app/src/main/java/com/devventure/sortnumber/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devventure.sortnumber
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,17 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dado01 = findViewById<TextView>(R.id.textView_dado2)
-        val dado02 = findViewById<TextView>(R.id.textView2_dado01)
+        val dado01 = findViewById<ImageView>(R.id.dado1)
+        val dado02 = findViewById<ImageView>(R.id.dado2)
         val btn = findViewById<Button>(R.id.button)
 
+        val images = listOf(R.drawable.dice_1,R.drawable.dice_2,R.drawable.dice_3,R.drawable.dice_4,R.drawable.dice_5,R.drawable.dice_6)
+
         btn.setOnClickListener{
-            dado01.text = sortNumber().toString()
-            dado02.text = sortNumber().toString()
+            dado01.setImageResource(images.random())
+            dado02.setImageResource(images.random())
         }
 
-        dado01.text = sortNumber().toString()
-        dado02.text = sortNumber().toString()
     }
     private fun sortNumber() : Int {
         return (1..6).random()
